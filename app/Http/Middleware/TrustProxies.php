ce8bc9c2baae2ad93b5454d139ac4977ec7f1a21
Mhmd-irsyd <irsyadmuhammad4321@gmail.com>
2025-07-15 19:35:36 +0700

    Set trusted proxies to '*' for HTTPS in production

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = '*'; // <--- BARIS INI YANG HARUS DIUBAH ATAU DITAMBAHKAN
 
     /**
      * The headers that should be used to detect proxies.
